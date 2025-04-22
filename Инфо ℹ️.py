f764c3e4e96d93a9ef165451ec8f0adb7c9cd995
RoboVorgen <69424004+RoboVorgen@users.noreply.github.com>
2025-04-22 19:06:31 +0300

    Update Инфо ℹ️.py

--- Инфо ℹ️.py
+++ Инфо ℹ️.py
@@ -3,30 +3,31 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
+def main():
+  st.set_page_config(page_title="Главная страница",
+                     layout = 'wide'
+                    )
+  
+  st.set_option('client.toolbarMode', 'developer')
+  st.set_option('client.showSidebarNavigation', True)
+  
+  
+  
+  st.write("## Данное web-приложение создано для работы с временными рядами")
+  
+  st.markdown("""
+          ### На данный момент в приложении реализован следующий функционал:
+      
+          * Отображение графиков
+          * Анализ временного ряда
+          * Прогнозирование ряда:
+              - наивными моделями
+              - трендовыми моделями
+              - моделями экспоненчиального сглаживания
+              - моделями ARIMA
+              - ~~моделями машинного обучения~~
+              - ~~нейронными сетями~~
+  """
+  )
 
-st.set_page_config(page_title="Главная страница",
-                   layout = 'wide'
-                  )
-
-st.set_option('client.toolbarMode', 'developer')
-st.set_option('client.showSidebarNavigation', True)
-
-
-
-st.write("## Данное web-приложение создано для работы с временными рядами")
-
-st.markdown("""
-        ### На данный момент в приложении реализован следующий функционал:
-    
-        * Отображение графиков
-        * Анализ временного ряда
-        * Прогнозирование ряда:
-            - наивными моделями
-            - трендовыми моделями
-            - моделями экспоненчиального сглаживания
-            - моделями ARIMA
-            - ~~моделями машинного обучения~~
-            - ~~нейронными сетями~~
-"""
-)
- 
+   
