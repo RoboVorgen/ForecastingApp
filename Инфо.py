45cc7fbb65d84dbbbab18ddd5ade9c724a9b5e53
RoboVorgen <69424004+RoboVorgen@users.noreply.github.com>
2025-05-09 17:05:27 +0300

    Update Инфо.py

--- Инфо.py
+++ Инфо.py
@@ -7,14 +7,9 @@ def main():
   st.set_page_config(page_title="Главная страница",
                      layout = 'wide'
                     )
-  
   st.set_option('client.toolbarMode', 'developer')
   st.set_option('client.showSidebarNavigation', True)
-  
-  
-  
   st.write("## Данное web-приложение создано для работы с временными рядами")
-  
   st.markdown("""
           ### На данный момент в приложении реализован следующий функционал:
       
